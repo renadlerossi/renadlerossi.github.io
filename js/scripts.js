60dda5d52002483d7c27cafc641d7632d4d2e62a
Renato Rossi <renadlerossi@gmail.com>
2017-02-15 03:59:14 +0000

    Create dynamic currency options on inputs.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,9 @@
+	// Acivate/Deactivate modal
 	function activateModal() {
 		document.getElementById("float").classList.toggle('active');
 	}
 
+	// Check code inputs completion to enable verify button
 	function checkForm() {
 		var f = document.forms["codeform"].elements;
 	    var cansubmit = true;
@@ -14,21 +16,46 @@
 	    document.getElementById('verify').disabled = !cansubmit;
 	}
 
+	// Format currency inputs
 	function formatForm(input) {
 		var nStr = input.value + '';
-		console.log(nStr);
 	    nStr = nStr.replace( /\,/g, "");
-	    console.log(nStr);
 	    x = nStr.split( '.' );
-	    console.log(x);
 	    x1 = x[0];
-	    console.log(x1);
 	    x2 = x.length > 1 ? '.' + x[1] : '';
-	    console.log(x2);
 	    var rgx = /(\d+)(\d{3})/;
-	    console.log(rgx);
 	    while ( rgx.test(x1) ) {
 	        x1 = x1.replace( rgx, '$1' + ',' + '$2' );
 	    }
 	    input.value = x1 + x2;
+	}
+
+	// Currency constructor
+	function currency(name, rate, flag, symbol) {
+	    this.Name = name;
+	    this.Rate = rate;
+	    this.Flag = flag;
+	    this.Symbol = symbol;
+	}
+
+	// Currencies Declaration
+	var currenciesArray = [];
+	var euro = new currency("EUR", 0.84, "images/eur.png", "€");
+	var pounds = new currency("GBP", 1.74, "images/gbp.png", "£");
+	currenciesArray.push(euro, pounds);
+
+	var selectSender = document.getElementById('sender-select');
+	for (element in currenciesArray){
+	    var opt = document.createElement('option');
+	    opt.value = element;
+	    opt.innerHTML = element.name;
+	    selectSender.appendChild(opt);
+	}
+
+	var selectReceiver = document.getElementById('receiver-select');
+	for (element in currenciesArray){
+	    var opt = document.createElement('option');
+	    opt.value = element;
+	    opt.innerHTML = element.name;
+	    selectReceiver.appendChild(opt);
 	}
